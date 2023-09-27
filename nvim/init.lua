a310fa9f5b629ec4c4f5d6cbe0ad01d56ed2b045
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-09-26 21:00:16 -0400

    nvim: lsp support + refactoring

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,89 +17,37 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Plugin includes.
 require('lazy').setup({
-  { 'sainnhe/gruvbox-material', priority = 1000 },
+  -- Colorscheme.
+  {
+    'sainnhe/gruvbox-material',
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.cmd([[colorscheme gruvbox-material]])
+    end,
+  },
+  -- Coq support.
   'whonore/Coqtail',
+  -- Unicode symbol input with latex commands.
   'joom/latex-unicoder.vim',
-  { 'nvim-telescope/telescope.nvim',
+  -- Fuzzy file/buffer/word finder.
+  {
+    'nvim-telescope/telescope.nvim',
     dependencies = {
+      -- Required telescope.vim dep. Helper module for async lua.
       'nvim-lua/plenary.nvim',
+      -- Optional telescope.vim dep. Fast fuzzy sorting.
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
   },
+  -- Sensible default configs for most LSP servers.
+  'neovim/nvim-lspconfig',
 })
 
--- Plugin: gruvbox-material (colorscheme).
-vim.cmd([[colorscheme gruvbox-material]])
-
--- Plugin: Coqtail (Coq).
--- Default maps leader keys in insert mode, which slows down normal leader typing.
-vim.g.coqtail_noimap = 1
--- Get rid of distracting CoqtailChecked bg highlight until I can find a better color.
-normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
-coq_hi_desc = vim.api.nvim_get_hl(0, { name = "CoqtailChecked" })
-coq_hi_desc.bg = normal_bg
-vim.api.nvim_set_hl(0, "CoqtailChecked", coq_hi_desc)
-
--- Plugin: latex-unicoder.vim (tex unicode).
--- Note: Std Latex symbs already exist in the package.
-iris = {}
-iris['fun']        = 'λ'
-iris['mult']       = '⋅'
-iris['ent']        = '⊢'
-iris['valid']      = '✓'
-iris['diamond']    = '◇'
-iris['box']        = '□'
-iris['bbox']       = '■'
-iris['later']      = '▷'
-iris['pred']       = 'φ'
-iris['and']        = '∧'
-iris['or']         = '∨'
-iris['comp']       = '∘'
-iris['ccomp']      = '◎'
-iris['all']        = '∀'
-iris['ex']         = '∃'
-iris['sep']        = '∗'
-iris['lc']         = '⌜'
-iris['rc']         = '⌝'
-iris['Lc']         = '⎡'
-iris['Rc']         = '⎤'
-iris['lam']        = 'λ'
-iris['empty']      = '∅'
-iris['Lam']        = 'Λ'
-iris['Sig']        = 'Σ'
-iris['-']          = '∖'
-iris['aa']         = '●'
-iris['af']         = '◯'
-iris['auth']       = '●'
-iris['frag']       = '◯'
-iris['gname']      = 'γ'
-iris['incl']       = '≼'
-iris['latert']     = '▶'
-iris['update']     = '⇝'
-iris['\'o']        = 'ö'
-iris['_a']         = 'ₐ'
-iris['_e']         = 'ₑ'
-iris['_h']         = 'ₕ'
-iris['_i']         = 'ᵢ'
-iris['_k']         = 'ₖ'
-iris['_l']         = 'ₗ'
-iris['_m']         = 'ₘ'
-iris['_n']         = 'ₙ'
-iris['_o']         = 'ₒ'
-iris['_p']         = 'ₚ'
-iris['_r']         = 'ᵣ'
-iris['_s']         = 'ₛ'
-iris['_t']         = 'ₜ'
-iris['_u']         = 'ᵤ'
-iris['_v']         = 'ᵥ'
-iris['_x']         = 'ₓ'
-iris[':']          = '∷'
-iris['not']        = '¬'
-iris['->']         = '→'
-iris['<->']        = '↔'
-iris['<-']         = '←'
-iris['\\notequiv'] = '≢'
-vim.g.unicode_map  = iris
+-- Some plugins need a lot of config, so move those to sep modules.
+-- Lua searches for these in `~/.config/nvim/lua`.
+require('nvim-lspconfig')
+require('coqtail')
 
 -- Plugin: telescope (fuzzy finder).
 require('telescope').load_extension('fzf')
@@ -123,18 +71,20 @@ vim.keymap.set('i', 'kj', '<Esc>')
 -- Misc Vim settings.
 vim.wo.relativenumber = true
 -- Set Vim clipboard to be the system clipboard.
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = 'unnamedplus'
 vim.o.ignorecase  = true
 vim.o.smartcase   = true
-vim.o.smarttab    = true
-vim.o.smartindent = true
 -- Start new horizontal split below current.
 vim.o.splitbelow  = true
 -- Start new vertical split to the right of current.
 vim.o.splitright  = true
+-- Some indentation settings I copied.
+-- Don't know if they are actually good.
+vim.o.smarttab    = true
+vim.o.smartindent = true
 vim.o.expandtab   = true
 vim.o.tabstop     = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth  = 4
--- Make trailing line spaces display as a "-".
+-- Make trailing line spaces display as a '-'.
 vim.opt.list = true
---
+++ nvim/lua/coqtail.lua
@@ -0,0 +1,68 @@
+-- Default maps leader keys in insert mode, which slows down normal leader typing.
+vim.g.coqtail_noimap = 1
+-- Get rid of distracting CoqtailChecked bg highlight until I can find a better color.
+normal_bg = vim.api.nvim_get_hl(0, { name = 'Normal' }).bg
+coq_hi_desc = vim.api.nvim_get_hl(0, { name = 'CoqtailChecked' })
+coq_hi_desc.bg = normal_bg
+vim.api.nvim_set_hl(0, 'CoqtailChecked', coq_hi_desc)
+
+-- Plugin: latex-unicoder.vim (tex unicode).
+-- Note: Std Latex symbs already exist in the package.
+iris = {}
+iris['fun']        = 'λ'
+iris['mult']       = '⋅'
+iris['ent']        = '⊢'
+iris['valid']      = '✓'
+iris['diamond']    = '◇'
+iris['box']        = '□'
+iris['bbox']       = '■'
+iris['later']      = '▷'
+iris['pred']       = 'φ'
+iris['and']        = '∧'
+iris['or']         = '∨'
+iris['comp']       = '∘'
+iris['ccomp']      = '◎'
+iris['all']        = '∀'
+iris['ex']         = '∃'
+iris['sep']        = '∗'
+iris['lc']         = '⌜'
+iris['rc']         = '⌝'
+iris['Lc']         = '⎡'
+iris['Rc']         = '⎤'
+iris['lam']        = 'λ'
+iris['empty']      = '∅'
+iris['Lam']        = 'Λ'
+iris['Sig']        = 'Σ'
+iris['-']          = '∖'
+iris['aa']         = '●'
+iris['af']         = '◯'
+iris['auth']       = '●'
+iris['frag']       = '◯'
+iris['gname']      = 'γ'
+iris['incl']       = '≼'
+iris['latert']     = '▶'
+iris['update']     = '⇝'
+iris['\'o']        = 'ö'
+iris['_a']         = 'ₐ'
+iris['_e']         = 'ₑ'
+iris['_h']         = 'ₕ'
+iris['_i']         = 'ᵢ'
+iris['_k']         = 'ₖ'
+iris['_l']         = 'ₗ'
+iris['_m']         = 'ₘ'
+iris['_n']         = 'ₙ'
+iris['_o']         = 'ₒ'
+iris['_p']         = 'ₚ'
+iris['_r']         = 'ᵣ'
+iris['_s']         = 'ₛ'
+iris['_t']         = 'ₜ'
+iris['_u']         = 'ᵤ'
+iris['_v']         = 'ᵥ'
+iris['_x']         = 'ₓ'
+iris[':']          = '∷'
+iris['not']        = '¬'
+iris['->']         = '→'
+iris['<->']        = '↔'
+iris['<-']         = '←'
+iris['\\notequiv'] = '≢'
+vim.g.unicode_map  = iris
