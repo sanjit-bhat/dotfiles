235666e7dbfef3e1bbf804a9b1ca9df1f5d046d3
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-07-18 11:48:36 -0400

    nvim: copy indent from jonhoo and copy some other nice looking settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -61,8 +61,6 @@ vim.keymap.set('n', '<leader>fb', function() tel.buffers() end)
 -- Misc key bindings.
 -- No highlight.
 vim.keymap.set('n', '<leader>h', '<cmd>noh<cr>')
--- From insert mode to normal mode.
-vim.keymap.set('i', 'kj', '<Esc>')
 -- On term open, go to insert mode so we can start running cmds.
 vim.api.nvim_create_autocmd({'TermOpen'}, {
   pattern = {'*'},
@@ -74,26 +72,47 @@ vim.keymap.set('n', '<leader>th', '<cmd>split term://zsh<cr>')
 vim.keymap.set('n', '<leader>tv', '<cmd>vsplit term://zsh<cr>')
 -- Window switch.
 vim.keymap.set('n', '<leader>w', '<C-w>')
+-- Make j and k move by visual line, not actual line, when text is soft-wrapped.
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+-- Jump to start and end of line using the home row keys.
+vim.keymap.set('', 'H', '^')
+vim.keymap.set('', 'L', '$')
+-- Ctrl+k as Esc.
+vim.keymap.set('n', '<C-k>', '<Esc>')
+vim.keymap.set('i', '<C-k>', '<Esc>')
+vim.keymap.set('v', '<C-k>', '<Esc>')
+vim.keymap.set('s', '<C-k>', '<Esc>')
+vim.keymap.set('x', '<C-k>', '<Esc>')
+vim.keymap.set('c', '<C-k>', '<Esc>')
+vim.keymap.set('o', '<C-k>', '<Esc>')
+vim.keymap.set('l', '<C-k>', '<Esc>')
+vim.keymap.set('t', '<C-k>', '<Esc>')
 
 -- Misc Vim settings.
-vim.wo.relativenumber = true
+-- Relative line numbers.
+vim.opt.relativenumber = true
+-- Show absolute line number for the current line.
+vim.opt.number = true
 -- Set Vim clipboard to be the system clipboard.
 vim.opt.clipboard = 'unnamedplus'
-vim.o.ignorecase  = true
-vim.o.smartcase   = true
+-- Case-insensitive search / replace.
+vim.opt.ignorecase = true
+-- Unless upper-case in search term.
+vim.opt.smartcase = true
 -- Start new horizontal split below current.
-vim.o.splitbelow  = true
+vim.opt.splitbelow = true
 -- Start new vertical split to the right of current.
-vim.o.splitright  = true
--- Some indentation settings I copied.
--- Don't know if they are actually good.
-vim.o.smarttab    = true
-vim.o.smartindent = true
-vim.o.expandtab   = true
-vim.o.tabstop     = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth  = 4
+vim.opt.splitright = true
 -- Make trailing line spaces display as a '-'.
 vim.opt.list = true
 -- Color a recommended column width.
 vim.opt.colorcolumn = "80"
+-- Number of spaces to use for each step of (auto)indent.
+vim.opt.shiftwidth = 4
+-- Number of spaces that a <Tab> counts for while performing editing operations.
+vim.opt.softtabstop = 4
+-- Number of spaces that a <Tab> in the file counts for.
+vim.opt.tabstop = 4
+-- Keep tabs as tabs, not spaces. Especially useful for Golang.
+vim.opt.expandtab = false
