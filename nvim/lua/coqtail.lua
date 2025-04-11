74c5b24d5e14431cd37e7eae1c66721e28e5a5b2
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-04-11 09:19:11 -0400

    rocq: better colors for checked / sent proofs

--- nvim/lua/coqtail.lua
+++ nvim/lua/coqtail.lua
@@ -1,10 +1,15 @@
--- Default maps leader keys in insert mode, which slows down normal leader typing.
+-- disable mapping leader key in insert mode. it slows down leader keypress.
 vim.g.coqtail_noimap = 1
 -- Auto-indenting within comments is buggy.
 -- E.g., writing "foo-bar" will trigger an indent bc of faulty "dash" interp.
 vim.g.coqtail_noindent_comment = 1
 -- Iris has a dune config, but it's buggy, so default to CoqProject.
 vim.g.coqtail_build_system = 'prefer-coqproject'
+-- use better colors for checked and sent proofs.
+vim.cmd([[
+highlight CoqtailChecked guibg=NvimLightGray1
+highlight CoqtailSent guibg=NvimLightGray3
+]])
 
 -- Plugin: latex-unicoder.vim (tex unicode).
 -- Note: Std Latex symbs already exist in the package.
