0468dbcceb9b76014132ff0a0255fc06acc8ca8c
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-12-23 10:38:54 -0500

    fish: a bit more config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,30 @@
-# No greeting when starting an interactive shell.
+# no greetings.
 function fish_greeting
 end
+
+# user_functions comes from dotfiles.
+# standard ~/.config/fish/functions is managed by fisher.
+set fish_function_path $__fish_config_dir/user_functions $fish_function_path
+
+# configure ecosystems.
+if [ -f /opt/homebrew/bin/brew ]
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+if [ -f /opt/homebrew/bin/rustup ]
+    fish_add_path (brew --prefix rustup)/bin
+end
+fish_add_path ~/.local/bin ~/.dotfiles/bin
+fish_add_path ~/.cargo/bin ~/go/bin (brew --prefix rustup)/bin
+
+direnv hook fish | source
+set -gx DIRENV_LOG_FORMAT
+
+# aliases.
+abbr --add g git
+abbr --add v nvim
+function wifi
+    ping google.com
+end
+
+# misc.
+set -gx EDITOR nvim
