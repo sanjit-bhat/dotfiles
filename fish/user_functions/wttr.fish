8bb96f5bc06ad92b906b47680bbc1bfd67d9e039
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-12-31 17:20:20 -0500

    wttr: add option for long report

--- fish/user_functions/wttr.fish
+++ fish/user_functions/wttr.fish
@@ -1,4 +1,15 @@
-function wttr -d "get the weather"
+function wttr -d "get the weather report"
+    argparse l/long -- $argv
+    or return
+
     # https://wttr.in/:help
-    curl wttr.in?0qT
+    # u: US units.
+    # 0: just current weather.
+    # q: quiet.
+    # T: no colors.
+    if set -ql _flag_long
+        curl wttr.in?uqT
+    else
+        curl wttr.in?u0qT
+    end
 end
