14dddc353b5791be251794ae2f639c2893f5d193
Sanjit Bhat <sanjit.bhat@gmail.com>
2026-05-18 11:59:43 -0400

    fish support linux homebrew

--- fish/config.fish
+++ fish/config.fish
@@ -10,31 +10,34 @@ set fish_function_path $__fish_config_dir/user_functions $fish_function_path
 if test -e /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
+if test -e /home/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
 
 # starship: shell prompt.
-if status --is-interactive; and test -e /opt/homebrew/bin/starship
+if status --is-interactive; and command -q starship
     starship init fish | source
 end
 
 # zoxide: fast dir switching.
-if status --is-interactive; and test -e /opt/homebrew/bin/zoxide
+if status --is-interactive; and command -q zoxide
     zoxide init fish | source
 end
 
 # direnv: custom .envrc files.
-if test -e /opt/homebrew/bin/direnv
+if command -q direnv
     direnv hook fish | source
     set -gx DIRENV_LOG_FORMAT
 end
 
 # colors for commands like fd.
-if test -e /opt/homebrew/bin/vivid
+if command -q vivid
     set -gx LS_COLORS (vivid generate gruvbox-light)
 end
 
 # configure ecosystems.
 fish_add_path ~/.local/bin ~/.cargo/bin ~/go/bin
-if test -e /opt/homebrew/bin/rustup
+if command -q rustup
     fish_add_path (brew --prefix rustup)/bin
 end
 if test -e ~/.opam/opam-init/init.fish
